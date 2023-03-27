2174e6462e2b0e6687c29c0e24952b5e9928617f
zhangxuejun <zhangxj12@mingyuanyun.com>
2023-03-27 22:38:51 +0800

    + 完成trino 开发工程搭建

---
+++ my-airlift/src/main/java/com/zuoye/my/airlift/resource/ServiceResource.java
@@ -0,0 +1,21 @@
+package com.zuoye.my.airlift.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author ZhangXueJun
+ * @Date 2023年03月27日
+ */
+@Path("/v1/service")
+public class ServiceResource
+{
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello()
+    {
+        return "Hello Airlift!";
+    }
+}
