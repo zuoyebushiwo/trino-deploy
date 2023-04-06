de4dea1faffa593f7905a6a0d4a5f4c64184bcba
zhangxuejun <zhangxj12@mingyuanyun.com>
2023-04-06 15:42:02 +0800

    + 完成trino-deploy-parse模块

--- trino-deploy-parser/src/main/java/io/trino/sql/tree/Identifier.java
+++ trino-deploy-parser/src/main/java/io/trino/sql/tree/Identifier.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.trino.sql.tree;
 
 import com.google.common.collect.ImmutableList;
@@ -11,13 +24,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
-/**
- * @author ZhangXueJun
- * @Date 2023年04月04日
- */
-public class Identifier extends Expression {
-
-
+public class Identifier
+        extends Expression
+{
     private static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z_]([a-zA-Z0-9_])*");
 
     private final String value;
@@ -68,12 +77,14 @@ public class Identifier extends Expression {
     }
 
     @Override
-    public <R, C> R accept(AstVisitor<R, C> visitor, C context) {
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
         return visitor.visitIdentifier(this, context);
     }
 
     @Override
-    public List<? extends Node> getChildren() {
+    public List<Node> getChildren()
+    {
         return ImmutableList.of();
     }
 
